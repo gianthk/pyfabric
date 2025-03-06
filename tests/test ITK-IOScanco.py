0dbb8ba3a8601324f9a83f6816cd72505ce78bf5
gianthk <gianthk.iori@gmail.com>
2025-03-06 13:36:56 +0100

    some ISQ tests

---
+++ tests/test ITK-IOScanco.py
@@ -0,0 +1,14 @@
+# test ITK-IOScanco
+
+import numpy as np
+# import pyfabric
+import os
+from ISQmethods import ISQload, readheader
+
+# STORAGE is the location of the data (on terminus, as mounted on Hari)
+STORAGE = "/Volumes/stamplab_terminus/external/tacosound/HR-pQCT_II/Vincent_hand"
+image = "C0001664.ISQ"
+
+tmp = readheader(os.path.join(STORAGE, image))
+
+print("here")
