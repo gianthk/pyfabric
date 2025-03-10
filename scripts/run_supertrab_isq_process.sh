933465ba91274324f7e87aa2c1090e94b4732b53
giiori <giiori@ethz.ch>
2025-03-10 16:41:04 +0100

    minor

--- scripts/run_supertrab_isq_process.sh
+++ scripts/run_supertrab_isq_process.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=pyfabric_%j
-#SBATCH --output=logs\pyfabric_%j.out
-#SBATCH --error=logs\pyfabric_%j.err
+#SBATCH --output=logs/pyfabric_%j.out
+#SBATCH --error=logs/pyfabric_%j.err
 #SBATCH --cpus-per-task=32
 #SBATCH --mem=512G
 #SBATCH --time=1:00:00
