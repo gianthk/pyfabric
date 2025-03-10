42fc8f1168a2c5e1317df5eb39c80ccfd4efe59a
giiori <giiori@ethz.ch>
2025-03-10 18:19:51 +0100

    increase time limit

--- scripts/run_supertrab_isq_process.sh
+++ scripts/run_supertrab_isq_process.sh
@@ -4,7 +4,7 @@
 #SBATCH --error=logs/pyfabric_%j.err
 #SBATCH --cpus-per-task=32
 #SBATCH --mem=512G
-#SBATCH --time=1:00:00
+#SBATCH --time=10:00:00
 
 # Variables section: 
 #export NUMEXPR_MAX_THREADS=10
